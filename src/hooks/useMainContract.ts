c6ee7e1d956bac152f61c0bdf6759a8c8e742be5
overdosed <r18537936420@gmail.com>
2024-08-28 20:54:39 +0800

    first commit

---
+++ src/hooks/useMainContract.ts
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { MainContract } from "../contracts/MainContract";
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { Address, OpenedContract, toNano } from "@ton/core";
+import { useTonConnect } from "./useTonConnect.ts";
+
+export function useMainContract() {
+  const client = useTonClient();
+  const [contractData, setContractData] = useState<null | {
+    counter_value: number;
+    recent_sender: Address;
+    owner_address: Address;
+  }>();
+
+  const [contract_balance, setContractBalance] = useState<number | null>(0);
+
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) =>
+    new Promise((resolve) => setTimeout(resolve, time));
+
+  const mainContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new MainContract(
+      Address.parse("kQCE_2ObbxqLkecQdm1zXhYItRBWZJVF3AQgut25Yg9UYgJV") // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<MainContract>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!mainContract) return;
+      setContractData(null);
+      const val = await mainContract.getData();
+      const {number} = await mainContract.getBalance();
+      setContractData({
+        counter_value: val.number,
+        recent_sender: val.recent_sender,
+        owner_address: val.owner_address,
+      });
+      setContractBalance(number);
+      await sleep(5000);
+      getValue();
+    }
+    getValue();
+  }, [mainContract]);
+
+  return {
+    contract_address: mainContract?.address.toString(),
+    contract_balance: contract_balance,
+    ...contractData,
+    sendIncrement: async () => {
+      return mainContract?.sendIncrement(sender, toNano("0.05"), 5);
+    },
+    sendDeposit: async () => {
+      return mainContract?.sendDeposit(sender, toNano("1"));
+    },
+    sendWithdrawalRequest: async () => {
+      return mainContract?.sendWithdrawlRequest(
+        sender,
+        toNano("0.05"),
+        toNano("0.7")
+      );
+    },
+  };
+}
